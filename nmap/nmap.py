93a4a1f03c09e979c4cdb962a2cff3b658bfa521
A.Norman (norman@xael.org) <A.Norman (norman@xael.org)>
2012-12-13 21:56:22 +0100

    error readability

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -788,9 +788,11 @@ class PortScannerError(Exception):
     def __init__(self, value):
         self.value = value
 
-
     def __str__(self):
         return repr(self.value)
+
+    def __repr__(self):
+        return 'PortScannerError exception {0}'.format(self.value)
 
 
 ############################################################################
