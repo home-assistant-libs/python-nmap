50921518eb8c9633b1dca1e1e90317d27a3249cf
A.Norman (norman@xael.org) <A.Norman (norman@xael.org)>
2013-09-24 17:54:49 +0200

    correction on exit status in example.py

--- nmap/example.py
+++ nmap/example.py
@@ -31,10 +31,10 @@ try:
     nm = nmap.PortScanner()         # instantiate nmap.PortScanner object
 except nmap.PortScannerError:
     print('Nmap not found', sys.exc_info()[0])
-    sys.exit(0)
+    sys.exit(1)
 except:
     print("Unexpected error:", sys.exc_info()[0])
-    sys.exit(0)
+    sys.exit(1)
 
 
 
