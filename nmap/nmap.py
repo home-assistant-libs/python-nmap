2bdfdbdf65e1dc7d80c75de047fdea737fb18ff9
Franck Nijhof <git@frenck.dev>
2021-06-29 13:21:51 +0200

    Bump version

--- nmap/nmap.py
+++ nmap/nmap.py
@@ -61,7 +61,7 @@ from xml.etree import ElementTree as ET
 
 
 __author__ = "Alexandre Norman (norman@xael.org)"
-__version__ = "0.7.0.1"
+__version__ = "0.7.0.2"
 __last_modification__ = "2021.06.29"
 
 
